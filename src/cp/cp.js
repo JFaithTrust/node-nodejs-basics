f537f97e3ad819c7ad6c3c49d93b43ed6ae6f39b
solijoniyjahongirmirzo <jahongirsolijoniy@gmail.com>
2025-10-21 19:33:05 +0500

    feat: implement child process spawning with argument handling

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,33 @@
+import { spawn } from "child_process";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const spawnChildProcess = async (args) => {
   // Write your code here
+  const scriptPath = path.join(__dirname, "files", "script.js");
+
+  const childProcess = spawn("node", [scriptPath, ...args], {
+    stdio: ["pipe", "pipe", "pipe"],
+  });
+
+  process.stdin.pipe(childProcess.stdin);
+
+  childProcess.stdout.pipe(process.stdout);
+  childProcess.stderr.pipe(process.stderr);
+
+  childProcess.on("exit", (code) => {
+    console.log(`Child process exited with code ${code}`);
+    process.exit(0);
+  });
+
+  childProcess.on("error", (error) => {
+    console.error(`Error spawning child process: ${error.message}`);
+    process.exit(1);
+  });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["arg1", "arg2"]);
