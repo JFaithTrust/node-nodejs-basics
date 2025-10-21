39b14bbae7c1e37be7b7684fac7b9f90adb7ac4a
solijoniyjahongirmirzo <jahongirsolijoniy@gmail.com>
2025-10-21 15:09:14 +0500

    feat: implement file renaming functionality

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,32 @@
+import { promises as fs } from "fs";
+import path from "path";
+
 const rename = async () => {
   // Write your code here
+  const dirPath = path.join(process.cwd(), "src/fs/files");
+  const oldPath = path.join(dirPath, "wrongFilename.txt");
+  const newPath = path.join(dirPath, "properFilename.md");
+
+  try {
+    const oldExists = await fs
+      .access(oldPath)
+      .then(() => true)
+      .catch(() => false);
+    const newExists = await fs
+      .access(newPath)
+      .then(() => true)
+      .catch(() => false);
+
+    if (!oldExists || newExists) {
+      throw new Error("FS operation failed");
+    }
+
+    await fs.rename(oldPath, newPath);
+
+    console.log("File renamed successfully!");
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 await rename();
