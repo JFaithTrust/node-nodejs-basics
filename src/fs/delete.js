1580fd9dce34415473d52721430bc9f027b3f5d0
solijoniyjahongirmirzo <jahongirsolijoniy@gmail.com>
2025-10-21 15:11:56 +0500

    feat: implement file deletion functionality

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,26 @@
+import { promises as fs } from "fs";
+import path from "path";
+
 const remove = async () => {
   // Write your code here
+  const filePath = path.join(process.cwd(), "src/fs/files/fileToRemove.txt");
+
+  try {
+    const fileExists = await fs
+      .access(filePath)
+      .then(() => true)
+      .catch(() => false);
+
+    if (!fileExists) {
+      throw new Error("FS operation failed");
+    }
+
+    await fs.unlink(filePath);
+
+    console.log("File removed successfully");
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 await remove();
