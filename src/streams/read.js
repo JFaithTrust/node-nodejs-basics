57c985cba29345c9d5802be018c69514c1775753
solijoniyjahongirmirzo <jahongirsolijoniy@gmail.com>
2025-10-21 18:15:41 +0500

    feat: implement file reading functionality using streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { createReadStream } from 'fs';
+import { resolve } from 'path';
+
 const read = async () => {
   // Write your code here
+  const filePath = resolve('src/streams/files/fileToRead.txt');
+
+  const stream = createReadStream(filePath, 'utf8');
+
+  stream.pipe(process.stdout);
+  stream.on('error', (err) => {
+    console.error('Error reading the file:', err.message);
+  });
 };
 
 await read();
