7e024c13cabb476991e8c1ddc1caa7d567915b93
solijoniyjahongirmirzo <jahongirsolijoniy@gmail.com>
2025-10-21 15:18:00 +0500

    feat: implement directory listing functionality

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,26 @@
+import { promises as fs } from "fs";
+import path from "path";
+
 const list = async () => {
   // Write your code here
+  const dirPath = path.join(process.cwd(), "src/fs/files");
+
+  try {
+    const dirExists = await fs
+      .access(dirPath)
+      .then(() => true)
+      .catch(() => false);
+
+    if (!dirExists) {
+      throw new Error("FS operation failed");
+    }
+
+    const files = await fs.readdir(dirPath);
+
+    console.log(files);
+  } catch (error) {
+    console.error(error.message);
+  }
 };
 
 await list();
